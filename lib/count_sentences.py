a0915c0f711300ee567e4d41ddd372ab34777ace
eugenelijondo <eugene.lijondo@student.moringaschool.com>
2024-03-29 11:06:44 +0300

    counter

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
+  def __init__(self, value):
+        self.value = value
+
+  def is_sentence(self):
+        if isinstance(self.value, str):
+            return self.value.endswith('.')
+        else:
+            print("The value must be a string.")
+            return False
+
+  def is_question(self):
+        if isinstance(self.value, str):
+            return self.value.endswith('?')
+        else:
+            print("The value must be a string.")
+            return False
+
+  def is_exclamation(self):
+        if isinstance(self.value, str):
+            return self.value.endswith('!')
+        else:
+            print("The value must be a string.")
+            return False
+
+  def count_sentences(self):
+        if isinstance(self.value, str):
+            return self.value.count('.') + self.value.count('?') + self.value.count('!')
+        else:
+            print("The value must be a string.")
+            return 0
   pass
